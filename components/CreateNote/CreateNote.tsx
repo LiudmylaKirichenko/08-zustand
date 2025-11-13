e792438cf00d76b6efc0cb6c765aa1ff801603d5
Liudmyla Kirichenko <liudmyla.deleychuk@gmail.com>
2025-11-13 18:52:44 +0200

    fix createNote component

--- components/CreateNote/CreateNote.tsx
+++ components/CreateNote/CreateNote.tsx
@@ -1,20 +1,12 @@
-"use client";
-import { useRouter } from "next/navigation";
 import NoteForm from "@/components/NoteForm/NoteForm";
-import css from "../CreateNote/CreateNote.module.css";
+import css from "./CreateNote.module.css";
 
 export default function CreateNote() {
-  const router = useRouter();
-
-  const handleClose = () => {
-    router.push("/notes/filter/All");
-  };
-
   return (
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
-        <NoteForm onClose={handleClose} />
+        <NoteForm />
       </div>
     </main>
   );
